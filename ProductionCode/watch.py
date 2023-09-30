6b803daaa267533d4b10f8224b2833ea4d47d43d
tccuong8 <chicuong04@gmail.com>
2023-09-30 15:28:46 -0500

    Add if __name__ == '__main__': main(). Move split_data_string_to_list into the CancerDataset class. Add codes to main() to instantiate a CancerDataset instance named 'dataset' and fill_it_with_cases().

--- ProductionCode/watch.py
+++ ProductionCode/watch.py
@@ -6,25 +6,11 @@ https://anyaevostinar.github.io/classes/257-f23/project-command-line
 
 import csv
 
-#not in final spot
-file = 'Data/dummy_file.csv'
-
-with open(file, 'r') as f: #opens csv file and reads is as a file
-    all_lines = f.readlines()
-    titles = all_lines.pop(0)   #removes first row of csv ans stores it in the list titles
-
-
-def split_data_string_to_list(string_with_commas):
-        """Split a string read from the .csv file into a convenient list, so it can be indexed. 
-        It also removes new line, if that's at the end of the last item.
-        If the list is empty, return empty list"""
-        try:
-            split_entry = string_with_commas.split(",")
-            #if split_entry[9][len(split_entry[9])-1] == "\n":
-            split_entry[-1] = split_entry[-1].strip("\n")
-        except IndexError:
-            split_entry = []
-        return split_entry
+# Drafts. Has been copied into use
+# file = 'Data/dummy_file.csv' -> main()
+# with open(file, 'r') as f: #opens csv file and reads is as a file ->  convert_dataset_into_titles_and_list_of_cases()
+#     all_lines = f.readlines()
+#     titles = all_lines.pop(0)   #removes first row of csv ans stores it in the list titles
 
 class Case:
     def __init__(self, state, year, leading_site, sex, count):
@@ -36,7 +22,7 @@ class Case:
     
     def case_details(self): pass
 
-class CancerDataset:  
+class CancerDataset: 
     def __init__(self, dataset_name):
         self.file = dataset_name # the .csv dataset file being accessed for the data
         self.list_of_cases = [] # list of cases, each of which is stored as a Case instance
@@ -47,22 +33,38 @@ class CancerDataset:
             all_lines = f.readlines() # get all the lines form the file
             return all_lines[0], all_lines[1:]   #removes first row of csv and returns it in the list titles
         
+    def split_data_string_to_list(string_with_commas):
+        """Split a string read from the .csv file into a convenient list, so it can be indexed. 
+        It also removes new line, if that's at the end of the last item.
+        If the list is empty, return empty list"""
+        try:
+            split_entry = string_with_commas.split(",")
+            #if split_entry[9][len(split_entry[9])-1] == "\n":
+            split_entry[-1] = split_entry[-1].strip("\n")
+        except IndexError:
+            split_entry = []
+        return split_entry
+    
     def fill_list_of_cases(self):
         """adds individual Case instances to the list"""
         titles, raw_data = self.convert_dataset_into_titles_and_list_of_cases()
         for i in range(len(raw_data)):
-            line_entry = split_data_string_to_list(raw_data[i])
+            line_entry = self.split_data_string_to_list(raw_data[i])
             case_entry = Case(line_entry[2],line_entry[3],line_entry[5],line_entry[7],line_entry[9])
             self.list_of_cases.append(case_entry)
+    
+    def convert_dataset_to_local_dataset(self):
+        self.convert_dataset_into_titles_and_list_of_cases(self)
 
     def get_data_from_year(year): pass
 
 # example code
 #case_example = Case("us", 2002, "mouth", "male", 2003)
 
+def main(): 
+    file = 'Data/dummy_file.csv'
+    dataset = CancerDataset(file) # now has a .list_of_cases = []
+    dataset.fill_list_of_cases() # now has a .list_of_cases = the whole file converted to list of Case instances
 
-
-
-
-def main(): pass
-
+if __name__ == '__main__':
+    main()
