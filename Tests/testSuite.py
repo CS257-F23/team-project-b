839018343df8eba18968f52d1bf0244344d0215c
tccuong8 <chicuong04@gmail.com>
2023-09-30 15:13:41 -0500

    Add failure message to test_make_lines and test_string_to_list_real_output. Add "from Data import *" so dummy_file.csv can be read by the unit tests. Add more details to the multi-string comments.

--- Tests/testSuite.py
+++ Tests/testSuite.py
@@ -1,9 +1,11 @@
 import unittest
+from Data import *
 from ProductionCode.watch import *
 
 
 class testFunctions(unittest.TestCase):
     def test_make_lines(self):
+        """Test to make sure that data can be fetched from .csv file and converted to list of CSV strings"""
         expected_list = ['Notes,States,States Code,Year,Year Code,Leading Cancer Sites,Leading Cancer Sites Code,Sex,Sex Code,Count\n', 
                          ",Alabama,1,2000,2000,Breast,26000,Female,F,2964\n",
                          ",Alabama,1,2003,2003,Myeloma,34000,Female,F,119\n",
@@ -16,14 +18,20 @@ class testFunctions(unittest.TestCase):
                          ',New York,36,2002,2002,Stomach,21020,Male,M,1088'
                          ]
         data_file = CancerDataset("dummy_file.csv")
-        output = data_file.make_lines()
-        self.assertEqual(output,expected_list)
+        output = data_file.convert_dataset_into_titles_and_list_of_cases()
+        self.assertEqual(output,expected_list,"Failed fetching the expected list and convert it to a list of CSV strings.")
 
     def test_string_to_list_real_output(self):
         """Tests to make sure that the string to list function works in a real use case"""
         example = ",Alabama,1,2000,2000,Breast,26000,Female,F,2964\n"
-        output = string_to_list(example)
-        self.assertEqual(output,["","Alabama","1","2000","2000","Breast","26000","Female","F","2964"])
+        output = split_data_string_to_list(example)
+        self.assertEqual(output,["","Alabama","1","2000","2000","Breast","26000","Female","F","2964"], "Failed to split a CSV line correctly.")
+    
+    def test_string_to_list_no_input(self):
+        """Tests to make sure that the string to list function works in an edge case of no input"""
+        example = ""
+        output = split_data_string_to_list(example)
+        self.assertEqual(output,[], "Failed to return empty list for no input.")
 
 
 # test that pop works
