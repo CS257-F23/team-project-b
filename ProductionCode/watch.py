0b329272ffa899a82f8aa00f76e8d361ea9771d3
johnsonm3 <johnsonm3@carleton.edu>
2023-10-02 20:11:39 -0500

    added desc. which shows up when user sends -h

--- ProductionCode/watch.py
+++ ProductionCode/watch.py
@@ -7,7 +7,7 @@ https://anyaevostinar.github.io/classes/257-f23/project-command-line
 import csv
 import argparse
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="A simple program that allows a user to access cancer data. To use, try using --year (a year between 2000-2000) or --site '(a cancer site)'. To see availible sites, try sending -h' ")
     #referenced realPython tutorial for CLI implementation.
     parser.add_argument("--year",type=int, choices=range(2000,2021))
     parser.add_argument("--site",type=str, choices=['Brain and Other Nervous System', 'Breast', 'Cervix Uteri', 'Colon and Rectum', 'Corpus Uteri', 'Esophagus', 'Gallbladder', 'Kidney and Renal Pelvis', 'Larynx', 'Leukemias', 'Liver', 'Lung and Bronchus', 'Melanoma of the Skin', 'Myeloma', 'Non-Hodgkin Lymphoma', 'Oral Cavity and Pharynx', 'Ovary', 'Pancreas', 'Prostate', 'Stomach', 'Thyroid', 'Urinary Bladder invasive and in situ'])
