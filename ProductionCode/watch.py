dbc2bb0b0d3498e4fc1b7ac0334314c179f990dd
johnsonm3 <johnsonm3@carleton.edu>
2023-10-23 19:22:30 -0500

    fixed minor errors on the home page and contact page, and improved docstrings for Production code files

--- ProductionCode/watch.py
+++ ProductionCode/watch.py
@@ -1,23 +1,12 @@
-"""
-A simple command line program that provides the user with data from the cdc US cancer database from 2000-2020.
-To use, send "python3 /ProductionCode/watch.py --(argument) --(data of interest)
-Currently, two arguments are supported:
---year (year between 2000-2020)
---site '(site)'
-*must choose from list of availible sites. to see which are availible, try using the -h command.
-Important links:
-https://docs.python.org/3/library/csv.html
-https://anyaevostinar.github.io/classes/257-f23/project-command-line
-"""
+"""A program that creates an instance of the CDC 2000-2020 dataset file for use with other parts of 
+the WATCH website project."""
 
 import csv
 from ProductionCode.cancerDataset_obj import *
 
-
 def main():
     global dataset
     dataset = CancerDataset("Data/clean_incidence.csv")
-
 
 if __name__ == "__main__":
     main()
--- cl.py
+++ cl.py
@@ -1,3 +1,15 @@
+"""
+A simple command line program that provides the user with data from the cdc US cancer database from 2000-2020.
+To use, send "python3 /ProductionCode/watch.py --(argument) --(data of interest)
+Currently, two arguments are supported:
+--year (year between 2000-2020)
+--site '(site)'
+*must choose from list of availible sites. to see which are availible, try using the -h command.
+Important links:
+https://docs.python.org/3/library/csv.html
+https://anyaevostinar.github.io/classes/257-f23/project-command-line
+"""
+
 from ProductionCode.watch import *
 import argparse
 import sys
