757ae8921b56cbd350e0824999e5da37931d2e80
johnsonm3 <johnsonm3@carleton.edu>
2023-11-04 13:10:29 -0500

    testing new method for simple search

--- app.py
+++ app.py
@@ -48,9 +48,12 @@ def display_number_of_matches(number_of_matches=0):
         target_site = "any_site"
         target_sex = "any_sex"
     all_input_as_one_URL_string = target_state + "," + target_year + "," + target_site + "," + target_sex
+    print(all_input_as_one_URL_string)
     target_state = parse_URL_string_to_list(all_input_as_one_URL_string)
     #TODO remove sort out, since we're just using a dropdown menu! [["state_name","Texas"], ["sex","Male"], ["leading_site","Liver"]]
     invalid_query_parameters, valid_column_and_query_parameters = sort_out_invalid_and_valid_query_parameters_with_column(target_state)
+    print(invalid_query_parameters)
+    print(valid_column_and_query_parameters)
     sql_for_number_of_matches = construct_multiargument_query_specified_targets("and",["SUM(case_count)"],valid_column_and_query_parameters)
     #TODO make a method in datasource that does the above; weird to call run_sql_command here
     number_of_matches = database.run_sql_command_and_return_result(sql_for_number_of_matches)
